9ce188c40186e76e6231858c4d310b1c86a4f01f
hienhan0901 <hienhan0901@gmail.com>
2022-01-13 00:33:17 +0700

    done UI

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,11 +1,302 @@
-import React from 'react'
+import React from "react";
+import "./dashboard.css";
+import { Link } from "react-router-dom";
 
 export default function Dashboard(props) {
-    
+  return (
+    <>
+      <div className="dashboardContainer">
+        <nav className="dashboardNavBar">
+          <span>Username |&nbsp;</span>
+          <Link to="/login">Log Out</Link>
+        </nav>
+        <nav className="dashboardNav">
+          <img className="dashboardNavImg" src="/icon1.png" alt="logo" />
+          <div className="dashboardNavSearch">
+            <input
+              type="text"
+              className="dashboardNavSearchBar"
+              placeholder="Search"
+            />
+            <ul className="dashboardNavUl">
+              <li>
+                <Link to="/" replace>
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div>
+                    <img src="/user-icon.png" alt="user" width="32px" />
+                    <div>John Smith </div>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="dashboardContent">
+          <div className="dashboardChatTitle">
+            <img src="/user-icon.png" alt="user" width="32px" />
+            <span>Title</span>
+          </div>
+          <div className="dashboardChatZone">
+          <div className="message">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                a djcsd sdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div><div className="message">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                a djcsd sdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div><div className="message">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                a djcsd sdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div><div className="message">
+              <span className="messageContent">
+              asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                a djcsd sdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
+            <div className="message mineMsg">
+              <span className="messageContent">
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+                a djcsd sdasdasd a fasdfadsbfj asdsda fasd as as
+                asdasdasd a fasdfadsbfj asdsda fasd as as
+              </span>
+              <span className="messageInfo">111</span>
+            </div>
 
-    return (
-        <>
-            dashboard
-        </>
-    )
+          </div>
+          <div className="dashboardChat">
+            <form className="dashboardChatForm">
+              <input
+                type="text"
+                className="dashboardChatInput"
+                placeholder="Aa"
+              />
+              <button className="dashboardChatButton">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./register.css";
+import "../login/login.css";
 
 export default function Register(props) {
   return (
     <>
-      <div className="registerContainer">
+      <div className="loginContainer">
         <img className="loginLogo" src="/echat-logo.png" alt="logo" />
-        <form className="registerForm">
+        <form className="loginForm">
           <input
             type="text"
             placeholder="Username"
             require
-            className="registerInput"
+            className="loginInput"
           />
           <input
             type="password"
             placeholder="Password"
             require
-            className="registerInput"
+            className="loginInput"
           />
           <input
             type="password"
             placeholder="Confirm password"
             require
-            className="registerInput"
+            className="loginInput"
           />
-          <button type="submit" className="registerButton">
+          <button type="submit" className="loginButton">
             Sign Up
           </button>
-          <span className=" registerSpan">
+          <span className=" loginSpan">
             Already have an account?&nbsp;
-            <Link to="/login" className="registerRegisterLink">
+            <Link to="/login" className="loginRegisterLink">
               Log in
             </Link>
           </span>
